0ca742f34990ad417ba8a613054b29571dccc1d8
Imon Islam <imon0077@yahoo.com>
2022-12-24 01:12:21 +0600

    Finalyze Add Product with custom validaiton for Inventories Price.

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using IMS.CoreBusiness.Validations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +20,23 @@ namespace IMS.CoreBusiness
         public int Quantity { get; set; }
 
         [Range(0, double.MaxValue, ErrorMessage = "Price must be greater or equal to {0}")]
+        [Product_EnsurePriceIsGreaterThanInventoriesPrice]
         public double Price { get; set; }
 
-        public List<ProductInventory> ProductInventories { get; set; }
+        public List<ProductInventory>? ProductInventories { get; set; }
+
+        public double TotalInventoryCost()
+        {
+            return this.ProductInventories.Sum(x => x.Inventory?.Price* x.InventoryQuantity ?? 0);
+        }
+
+        public bool ValidatePricing() {
+            //we are not validating the product object when the product inventories are not loaded.
+            if (ProductInventories == null || ProductInventories.Count <= 0) return true;
+
+            if(this.TotalInventoryCost() > Price) return false;
+
+            return true;
+        }
     }
 }
---
+++ IMS.CoreBusiness/Validations/Product_EnsurePriceIsGreaterThanInventoriesPrice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.CoreBusiness.Validations
+{
+    internal class Product_EnsurePriceIsGreaterThanInventoriesPrice : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var product = validationContext.ObjectInstance as Product;
+            if (product != null)
+            {
+                if (!product.ValidatePricing())
+                {
+                    return new ValidationResult($"The product's price is less than the summary of its inventories price: {product.TotalInventoryCost()}", 
+                        new[] { validationContext.MemberName});
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
